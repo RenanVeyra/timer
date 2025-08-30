b96a6a2adc3aa8ed810f3bd349794cb8d14468a7
Jitmanyu Sk <jit@Jitmanyus-MacBook-Air.local>
2025-08-30 11:21:54 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter_randomcolor/flutter_randomcolor.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  //bool darkMode = false;
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
---
+++ lib/screens/displayScreen.dart
@@ -0,0 +1,19 @@
+// ignore: file_names
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class Displayscreen extends ConsumerWidget {
+  const Displayscreen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final String value = ref.watch(helloWorldProvider);
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Example')),
+        body: Center(child: Text(value)),
+      ),
+    );
+  }
+}
